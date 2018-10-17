9906b9a5fd1d5af58e80589b3ca57f7f22abce52
seungma93 <seungma93@naver.com>
2018-10-17 15:48:30 +0900

    10-17

--- src/main/java/com/seungma93/webservice/domain/posts/Posts.java
+++ src/main/java/com/seungma93/webservice/domain/posts/Posts.java
@@ -18,7 +18,7 @@ import javax.persistence.Id;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
